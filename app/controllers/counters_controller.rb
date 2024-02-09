991152b59e7cb939fb489788ed0f479ce57fafa3
Tsubata Seisyu <seisyuu43@gmail.com>
2024-02-09 10:19:12 +0900

    不要コード削除

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -6,7 +6,6 @@ class CountersController < ApplicationController
         max_seats = 100 
         counter = Counter.last || Counter.create(count: 0)
         @congestion_rate = (counter.count.to_f / max_seats * 100).floor # 小数点以下切り捨て
-        # @congestion_rate = 30
     end
 
     def increase
