30c92e66017e0cad01aebef4ab3d8b90262e0465
Tsubata Seisyu <seisyuu43@gmail.com>
2024-02-09 10:12:42 +0900

    条件分岐追加

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -1,8 +1,12 @@
 class CountersController < ApplicationController
+
+    protect_from_forgery except: [:increase, :decrease]
+
     def index
         max_seats = 100 
         counter = Counter.last || Counter.create(count: 0)
         @congestion_rate = (counter.count.to_f / max_seats * 100).floor # 小数点以下切り捨て
+        # @congestion_rate = 30
     end
 
     def increase
